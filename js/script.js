32005a3381736cc1d7db092986cd93c3da47e7e5
Aleks <i.aleksey>
2024-03-30 12:45:24 +0300

    http request

--- js/script.js
+++ js/script.js
@@ -12,6 +12,28 @@ inputRub.addEventListener('input', () => {
    const request = new XMLHttpRequest();
 
    //метод сбора настроек, формат: request.open(method, url, async, login, pass)
-   request.open('GET',);
+   request.open('GET', 'js/current.json');
+
+   //http заголовки
+   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+   //отправка запроса
+   request.send();
+
+   // свойства полученных данных
+   // status
+   // statusText
+   // response
+   // readyState
+
+   //события
+   request.addEventListener('load', () => {
+      if (request.status === 200) {
+         const data = JSON.parse(request.response);
+         inputUsd.value = (+inputRub.value / data.current.usd).toFixed(3);
+      } else {
+         inputUsd.value = 'error';
+      }
+   });
 
 });
