c2d972bc49c8fb7fdff16d59c35aef79661a706d
Aleks <i.aleksey>
2024-04-03 23:27:56 +0300

    promise

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //======================AJAX========================
-
+/*
 // 1) XMLHttpRequest - встроенный объект в браузер (не актуально)
 
 const inputRub = document.querySelector('#rub');
@@ -35,5 +35,61 @@ inputRub.addEventListener('input', () => {
          inputUsd.value = 'error';
       }
    });
-
 });
+*/
+
+//======================PROMISE============================
+console.log('Запрос данных...');
+const req = new Promise((resolve, reject) => {
+   setTimeout(() => {
+      console.log('Подготовка данных...');
+
+      const product = {
+         name: 'TV',
+         price: 2000
+      };
+
+      resolve(product);
+   }, 2000);
+});
+
+req.then((product) => {
+   return new Promise((resolve, reject) => {
+      setTimeout(() => {
+         product.status = 'order';
+         resolve(product);
+      }, 2000);
+   });
+}).then(data => {
+   data.modify = true;
+   return data;
+}).then(data => {
+   console.log(data);
+}).catch(() => {
+   console.error('Произошла ошибка');
+}).finally(() => {
+   console.log('Finally');
+});
+
+const test = time => {
+   return new Promise(resolve => {
+      setTimeout(() => {
+         resolve();
+      }, time);
+   });
+};
+
+//test(1000).then(() => console.log('1000 ms'));
+//test(2000).then(() => console.log('2000 ms'));
+
+//Ждет выполнения всех промисов
+Promise.all([test(1000), test(2000)]).then(() => {
+   console.log('All');
+});
+
+//Ждет выполнения первого промиса
+Promise.race([test(1000), test(2000)]).then(() => {
+   console.log('race');
+});
+
+
