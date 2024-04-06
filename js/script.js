ed5ad41852078e2d52e56ed9a29058ff6c39f462
Aleks <i.aleksey>
2024-04-06 15:34:47 +0300

    tasks

--- js/script.js
+++ js/script.js
@@ -152,3 +152,89 @@ const newArr = Object.entries(obj)
    .map(item => item[0]);
 console.log(newArr);
 */
+/*
+//===================Задачи======================
+const films = [
+   {
+      name: 'Titanic',
+      rating: 9
+   },
+   {
+      name: 'Die hard 5',
+      rating: 5
+   },
+   {
+      name: 'Matrix',
+      rating: 8
+   },
+   {
+      name: 'Some bad film',
+      rating: 4
+   }
+];
+
+function showGoodFilms(arr) {
+   return arr.filter(item => item.rating >= 8);
+}
+console.log(showGoodFilms(films));
+
+
+
+function showListOfFilms(arr) {
+   return (arr.map(item => item.name)).join(', ');
+}
+console.log(showListOfFilms(films));
+
+function showListOfFilms2(arr) {
+   return (arr.reduce((acc, item) => `${typeof (acc) === 'object' ? acc.name : acc}, ${item.name}`));
+}
+console.log(showListOfFilms2(films));
+
+
+
+function setFilmsIds(arr) {
+   return arr.reduce(function (acc, item, index, arr) {
+      item.id = index;
+      return arr;
+   }, 0);
+}
+console.log(setFilmsIds(films));
+
+function setFilmsIds2(arr) {
+   return arr.map((item, index) => {
+      item.id = index;
+      return item;
+   })
+}
+console.log(setFilmsIds2(films));
+
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+   return arr.every(item => 'id' in item);
+}
+
+console.log(checkFilms(tranformedArray));
+*/
+/*
+//===================Задачи======================
+const funds = [
+   { amount: -1400 },
+   { amount: 2400 },
+   { amount: -1000 },
+   { amount: 500 },
+   { amount: 10400 },
+   { amount: -11400 }
+];
+
+const getPositiveIncomeAmount = (data) => {
+   return data.filter(item => item.amount > 0).reduce((acc, item) => acc + item.amount, 0);
+};
+console.log(getPositiveIncomeAmount(funds));
+
+const getTotalIncomeAmount = (data) => {
+   return data.some(item => item.amount < 0) ? data.reduce((acc, curr) => acc + curr.amount, 0) : getPositiveIncomeAmount(data);
+};
+console.log(getTotalIncomeAmount(funds));
+*/
