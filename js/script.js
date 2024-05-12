a230daba3e2ce6048d486f0b672a1193ac60b5af
Aleks <i.aleksey>
2024-05-12 19:46:40 +0300

    generator

--- js/script.js
+++ js/script.js
@@ -532,3 +532,35 @@ try {
 
 }
 */
+/*
+//=======================Функции-генераторы=========================
+function* generator() {
+   yield 'S';
+   yield 'c';
+   yield 'r';
+}
+
+const str = generator();
+console.log(str.next()); //{ value: 'S', done: false }
+console.log(str.next()); //{ value: 'c', done: false }
+console.log(str.next()); //{ value: 'r', done: false }
+console.log(str.next()); //{ value: undefined, done: true }
+
+const strValue = generator();
+console.log(strValue.next().value); //S
+
+function* count(n) {
+   for (let i = 0; i < n; i++) {
+      yield i;
+   }
+}
+
+const counter = count(7);
+
+console.log(counter.next().value); //0
+console.log(counter.next().value); //1
+
+for (let k of count(5)) {
+   console.log(k); //0 1 2 3 4
+}
+*/
